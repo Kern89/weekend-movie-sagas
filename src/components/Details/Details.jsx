384a711d410715d987c6aaf8e528b329de15a87e
Kenneth Grina <grina.kenneth@gmail.com>
2024-05-06 18:19:58 -0500

    clicking on movie brings you to /id path

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,8 +1,13 @@
-
+import { Button } from "@mui/material";
+import { useHistory } from "react-router-dom"
 function DetailsView() {
-
+    const history = useHistory();
+    const goToMovieList = () => {
+        history.push('/')
+    }
     return(
         <>
+        <Button onClick={goToMovieList} data-testid="toList" >Back to movie list</Button>
         </>
     )
 };
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './MovieList.css';
 
 function MovieList() {
-
+  const history = useHistory();
   const dispatch = useDispatch();
   const movies = useSelector(store => store.movies);
 
@@ -11,15 +12,20 @@ function MovieList() {
     dispatch({ type: 'FETCH_MOVIES' });
   }, []);
 
+  const toDetails = (id) => {
+    history.push(`/${id}`)
+  };
   return (
     <main>
       <h1>MovieList</h1>
       <section className="movies">
         {movies.map(movie => {
           return (
-            <div data-testid='movieItem' key={movie.id}>
+            <div data-testid='movieItem' key={movie.id}
+              onClick={(e) => {toDetails(movie.id)}}
+            >
               <h3>{movie.title}</h3>
-              <img src={movie.poster} alt={movie.title}/>
+              <img src={movie.poster} alt={movie.title} data-testid="toDetails"/>
             </div>
           );
         })}
